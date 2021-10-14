00b65f67598f14c3157e47a7016a2e69601daa67
esmoreido <moreido@mail.ru>
2021-10-14 11:34:19 +0300

    goog-looking app

--- qrank/app.R
+++ qrank/app.R
@@ -2,23 +2,37 @@ library(shiny)
 library(ggplot2)
 library(bibliometrix)
 library(DT)
+library(dplyr)
 
 ui <- fluidPage(
     titlePanel("Подсчет количества статей в журналах различных квартилей SJR"),
-    sidebarLayout(
-        sidebarPanel(
-            fileInput("csvfile", "Выберите файл *.bib", buttonLabel = "Выбрать...", 
-                      placeholder = "",                      
-                      accept = "bib"
-            )
-            # tags$hr(),
-            # checkboxInput("header", "Header", TRUE)
+    fluidRow(
+        column(4,
+               fileInput("csvfile", 
+                         "Выберите файл *.bib", 
+                         buttonLabel = "Выбрать...", 
+                                           placeholder = "",
+                                           accept = "bib"
+                                 )
+               ),
+        column(8,
+               br(),
+               "Для загрузки используйте файлы, полученные 
+               экспортом интересующих записей из поиска Scopus
+               в формате BibTeX.")
+    ),
+    fluidRow(
+        column(6,
+               plotOutput("plot")
+                    ),
+        column(6, 
+               tableOutput("count")
+               )
         ),
-        mainPanel(
-            DT::dataTableOutput("table"),
-            plotOutput("plot"),
-            tableOutput("count")
-        )
+    fluidRow(
+        column(12,
+               DT::dataTableOutput("table")     
+               )
     )
 )
 
@@ -36,27 +50,35 @@ server <- function(input, output, session) {
         rownames(rec) <- NULL
         df <- merge(rec[,c(1, 4, 5, 10, 13)], sjr[, c(3, 7)], by.x ='SO', by.y = 'Title', all.x = T)
         df <- df[order(df$PY, decreasing = T),]
+        rownames(df) <- NULL
+        print(colnames(df))
+        colnames(df) <- c('Журнал', 'Авторы', 'DOI', 'Название', 'Год', 'Квартиль')
         # print(summary(df))
         return(df)
     })
     
-    output$table <- DT::renderDataTable({
-        outTable()
-        })
+    output$table <- DT::renderDataTable(
+        outTable(),
+        options = list(
+            language = list(url = "https://cdn.datatables.net/plug-ins/1.11.3/i18n/ru.json")
+        )
+        )
     
     output$plot <- renderPlot({
         df <- outTable()
-        ggplot(df, aes(x=SJR.Best.Quartile, fill=SJR.Best.Quartile)) +
+        ggplot(df, aes(x=`Квартиль`, fill=`Квартиль`)) +
             geom_histogram(stat = 'count') +
-            stat_count(aes(y=..count..,label=..count..),geom="text",vjust=-1) +
-            labs(title = paste('SCIMAGO JR ', min(df$PY), '-', max(df$PYD)), x='Квартиль', y='Количество',
-                 fill='Квартиль') + theme_light(base_size = 20)
+            stat_count(aes(y=..count..,label=..count..),geom="text",vjust=1) +
+            labs(title = paste('SCIMAGO JR ', min(df$`Год`, na.rm = T), '-', 
+                               max(df$`Год`, na.rm = T)), x='Квартиль', y='Количество статей',
+                 fill='Квартиль') + theme_void(base_size = 20)
     })
     output$count <- renderTable({
         df <- outTable()
         df %>%
-            group_by(df$SJR.Best.Quartile) %>%
-            summarise(articles = n())
+            dplyr::group_by(df$`Квартиль`) %>%
+            dplyr::summarise(articles = dplyr::n()) %>%
+            `colnames<-`(c('Квартиль', 'Количество статей'))
         
     })
 }
